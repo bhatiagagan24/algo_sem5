3800b82444c46c54e32ff19e04e54bc89f084a5e
bhatiagagan24 <47947926+bhatiagagan24@users.noreply.github.com>
2020-08-17 10:39:19 +0530

    Update binary_search.cpp

--- binary_search/binary_search.cpp
+++ binary_search/binary_search.cpp
@@ -5,27 +5,27 @@
 
 using namespace std;
 
-int finding_element(int arr[], int element, int no)
+int binary_find(int arr[], int s, int a, int elem)
 {
-    int x = 0;
-    int a = no; // a = number of elements
-    int b = element; // b = element we are searching
-    if(a%2 == 0) // it means no of element are even
-    {
-        int x = a / 2;
-        if (arr[x] == b){
-            return x;
-        }
+    if (a >= s) {
+        int mid = s + (a - s) / 2;
+        if (arr[mid] == elem)
+            return mid;
+        if (arr[mid] < elem)
+            return binary_find(arr, mid + 1, a, elem);
+        else
+        return binary_find(arr, s, mid - 1, elem);
+
     }
-   return 1;
+    return -1;
 }
 int main()
 {
-    int element = 3;
+    int element = 1;
     int arr[] = { 1, 3, 5, 7, 8, 900, 1000000, 34} ;
     int a = end(arr) - begin(arr); // here a = no of elements in arr
-    int m = finding_element(arr, a, element);
-    if (m==1){
+    int m = binary_find(arr, 0, a, element);
+    if (m== -1){
         cout<<"element not found";
     }
     else{
